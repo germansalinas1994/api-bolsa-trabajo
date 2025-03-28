c5ecd95905f5a5c264ebbae2c727ff969c98e6dc
German Salinas <germansalinas.fce@gmail.com>
2025-03-28 11:40:12 -0300

    base de datos en railway

--- DataAccess/Entities/Db_NOMBRE_BASE_Context.cs
+++ DataAccess/Entities/Db_NOMBRE_BASE_Context.cs
@@ -24,7 +24,7 @@ public partial class Db_NOMBRE_BASE_Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=localhost;port=3306;database=mydb;uid=root;password=12345678;");
+        => optionsBuilder.UseMySQL("server=turntable.proxy.rlwy.net;port=50549;database=mydb;uid=root;password=PEHhIOYJDNthnMkOamikDnjcBELDlwOA;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
