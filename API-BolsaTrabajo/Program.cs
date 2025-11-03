57b80a6325bae43113ae28398732fa66e5b42cdf
GonzaloEBaez <gezbaez@gmail.com>
2025-11-02 22:33:10 -0300

    bd gonza

--- API-BolsaTrabajo/Program.cs
+++ API-BolsaTrabajo/Program.cs
@@ -73,8 +73,8 @@ builder.Services.AddSwaggerGen(c =>
 // builder.Services.AddDbContext<DbBolsaTrabajoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection")));
 
 //base de datos local
-// builder.Services.AddDbContext<DbBolsaTrabajoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection-Gonza")));
-builder.Services.AddDbContext<DbBolsaTrabajoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection-notebook")));
+builder.Services.AddDbContext<DbBolsaTrabajoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection-Gonza")));
+// builder.Services.AddDbContext<DbBolsaTrabajoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection-notebook")));
 
 //agrego la inyeccion de dependencia de los repositorios y el UnitOfWork
 
